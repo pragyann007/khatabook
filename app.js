8accd057c4c8a0d6eafbddabd09849fdd97bb2e9
Pragyan <pragyanthapaliya027@gmail.com>
2024-11-12 07:39:27 -0800

    initalising

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
 
+// Set up EJS as the view engine
+app.set("view engine", "ejs");
+
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the "public" folder
 app.use(express.static('public'));
-app.set("view-engine", "ejs");
 
-app.get('/',(req,res)=>{
-    try{
-        res.render('index');
+// Define a route for the homepage
+app.get('/', (req, res) => {
+    try {
+        res.render('index'); 
+    } catch (err) {
+        res.send("Something went wrong");
     }
-    catch(err){
-        res.send("Something went wrong",err.message)
-    }
-})
+});
 
-app.listen('3000',()=>{
-    console.log("Server is running on port 3000")
-})
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
